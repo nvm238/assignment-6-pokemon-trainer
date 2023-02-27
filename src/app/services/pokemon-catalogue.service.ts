a7add1bf3bcc06d02e8e24d6ad7bf202955cb31e
NVM238 <nielsvm@live.nl>
2023-02-27 14:22:29 +0100

    Feat: create pokemon list

---
+++ src/app/services/pokemon-catalogue.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { finalize } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Pokemon } from '../models/pokemon.model';
+
+const { apiPokemons } = environment; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonCatalogueService {
+
+  private _pokemons: Pokemon[] = [];
+  private _error: string = "";
+  private _loading: boolean = false;
+
+  get pokemons(): Pokemon[] {
+    return this._pokemons;
+  }
+
+  get error(): string {
+    return this._error;
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
+
+  constructor(private readonly http: HttpClient) { }
+
+  public findAllPokemons(): void {
+    this._loading = true;
+    this.http.get<Pokemon[]>(apiPokemons)
+      .pipe(
+        finalize(() => {
+          this._loading = false;
+        })
+      )
+      .subscribe({
+        next: (pokemons: Pokemon[]) => {
+          this._pokemons = pokemons;
+        },
+        error: (error: HttpErrorResponse) => {
+          this._error = error.message;
+        }
+      })
+  }
+}
